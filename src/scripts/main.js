860dccb25af705d70971b61ac86458f07f72ee3e
Zaransoftline <131307196+Zaransoftline@users.noreply.github.com>
2025-01-29 20:08:41 -0500

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,48 @@
 'use strict';
+
+let leftClick = false;
+let rightClick = false;
+
+function displayMessage(message) {
+  const div = document.createElement('div');
+
+  div.textContent = message;
+  document.body.appendChild(div);
+}
+
+const leftClickPromise = new Promise((resolve) => {
+  document.addEventListener(
+    'click',
+    () => {
+      if (!leftClick) {
+        leftClick = true;
+        displayMessage('First promise was resolved');
+        resolve();
+      }
+    },
+    { once: true },
+  );
+});
+
+const rightClickPromise = new Promise((resolve) => {
+  document.addEventListener(
+    'contextmenu',
+    () => {
+      if (!rightClick) {
+        rightClick = true;
+        displayMessage('Second promise was resolved');
+        resolve();
+      }
+    },
+    { once: true },
+  );
+});
+
+const bothClicksPromise = Promise.all([
+  leftClickPromise,
+  rightClickPromise,
+]).then(() => {
+  displayMessage('Third promise was resolved');
+});
+
+bothClicksPromise.then(() => {});
